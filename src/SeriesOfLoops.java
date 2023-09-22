e446aa4fed1929010c1af9e3774546f2cf6a275a
Jenn <jenniferjuarezjj@gmail.com>
2023-09-22 13:59:17 -0400

    create a for loop that displays a string 99 times

--- src/SeriesOfLoops.java
+++ src/SeriesOfLoops.java
@@ -1,5 +1,13 @@
 public class SeriesOfLoops {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // in class we are learned about for loops
+        // i will have the instructions commented out and any other questions i may have...
+        //->
+
+        //below display a loop that displayed a string 99 times
+        for (int i = 0; i < 99; i++) {
+            System.out.println(" This is string number: " + (i + 1));
+        }
+
     }
 }
