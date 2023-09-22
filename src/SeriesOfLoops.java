7fe9c8a5a9c9ede33f99242766113f16a499be58
Jenn <jenniferjuarezjj@gmail.com>
2023-09-22 14:23:43 -0400

    create a for loop that adds all the odd nums

--- src/SeriesOfLoops.java
+++ src/SeriesOfLoops.java
@@ -4,26 +4,33 @@ public class SeriesOfLoops {
         // i will have the instructions commented out and any other questions i may have...
         //->
 
-        //below display a loop that displayed a string 99 times
+        //1. below display a loop that displayed a string 99 times
         for (int i = 0; i < 99; i++) {
             System.out.println(" This is string number: " + (i + 1));
         }
 
-        //a loop that will display every odd number from 1 to 99
+        //2. a loop that will display every odd number from 1 to 99
         for (int i = 1; i <= 99; i += 2) {
             System.out.println("The odd number displayed in this loop is: " + i);
         }
 
-        //a loop that will display every even number 0 to 99
+        //3. a loop that will display every even number 0 to 99
         for (int i = 0; i <= 99; i += 2) {
             System.out.println("The even number displayed in this loop is: " + i );
         }
 
-        //a loop that will display ever even number from 0 to 99 a different way
+        //4. a loop that will display ever even number from 0 to 99 a different way
         for (int i = 0; i <= 99; i++) {
             if (i % 2 == 0) {
                 System.out.println(" The even number displayed in the second version of the even number loop is: " + i );
             }
         }
+
+        //5. a loop that will sum up the odd numbers from 0 to 99 and print it at the end of the loop
+        int sumOdd = 1;
+        for (int i = 1; i <= 99; i += 2) {
+            sumOdd += 1;
+        }
+        System.out.println("The sum of odd numbers from numbers 0 to 99 is..." + sumOdd);
     }
 }
